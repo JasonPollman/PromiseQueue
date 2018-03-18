88cc3a78a167c935f548e1e4f63a35ea6acf71e5
Jason James Pollman <jasonjpollman@gmail.com>
2018-03-17 23:28:22 -0400

    Started integration tests

---
+++ test/PromiseQueueIntegration.test.js
@@ -0,0 +1,42 @@
+import { expect } from 'chai';
+import { exec } from 'child_process';
+import PromiseQueue from '../dist/PromiseQueue';
+
+const execAsync = (...args) => new Promise((resolve, reject) => (
+  exec(...args, (err, results) => (err ? reject(err) : resolve(results)))
+));
+
+describe('Integration', () => {
+  let queuedExec;
+
+  beforeEach(() => {
+    queuedExec = PromiseQueue.queueify(execAsync);
+  });
+
+  it('Should only run one command at a time (in order)', () => Promise.all([
+    queuedExec('ls', { cwd: __dirname }),
+    queuedExec('touch test.txt', { cwd: __dirname }),
+    queuedExec('ls', { cwd: __dirname }),
+    queuedExec('rm ./test.txt', { cwd: __dirname }),
+    queuedExec('ls', { cwd: __dirname }),
+  ]).then(([a, b, c, d, e]) => {
+    expect(a.trim().split(/\r?\n+/g)).to.eql([
+      'PromiseQueue.test.js',
+      'PromiseQueueIntegration.test.js',
+      'mocha.opts',
+    ]);
+    expect(b.trim()).to.equal('');
+    expect(c.trim().split(/\r?\n+/g)).to.eql([
+      'PromiseQueue.test.js',
+      'PromiseQueueIntegration.test.js',
+      'mocha.opts',
+      'test.txt',
+    ]);
+    expect(d.trim()).to.eql('');
+    expect(e.trim().split(/\r?\n+/g)).to.eql([
+      'PromiseQueue.test.js',
+      'PromiseQueueIntegration.test.js',
+      'mocha.opts',
+    ]);
+  }));
+});
